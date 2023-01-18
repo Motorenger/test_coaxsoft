6f5a3d1e0a7af549208fcc2a375228421344e047
Motorenger <90602764+Motorenger@users.noreply.github.com>
2023-01-18 21:17:06 +0200

    Created all models and made migrations also covered admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Categories, Products, Orders
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'user', 'price',)
+    search_fields = ['category__name']
+
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'product_name', 'category', 'price')
+
+
+admin.site.register(Categories)
+admin.site.register(Products, ProductsAdmin)
+admin.site.register(Orders, OrdersAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=150)
+
+    class Meta:
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    category = models.ForeignKey(Categories, related_name="products", on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), related_name="products", on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
+
+    def __str__(self):
+        return self.name
+
+
+class Orders(models.Model):
+    username = models.CharField(max_length=150)
+    email = models.EmailField(max_length=254)
+    product_name = models.CharField(max_length=150)
+    category = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        verbose_name = _("Order")
+        verbose_name_plural = _("Orders")
+
+    def __str__(self):
+        return f"{self.username}'s order"
